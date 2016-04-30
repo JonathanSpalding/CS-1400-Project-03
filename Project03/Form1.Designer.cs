846a87b3eb62b86f8b492cd6371350256e31fa73
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-30 10:15:16 -0600

    CS 1400 C#

---
+++ Project03/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Project03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startTime = new System.Windows.Forms.TextBox();
+            this.endTime = new System.Windows.Forms.TextBox();
+            this.arrivalTime = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startTime
+            // 
+            this.startTime.Location = new System.Drawing.Point(280, 69);
+            this.startTime.Name = "startTime";
+            this.startTime.Size = new System.Drawing.Size(100, 29);
+            this.startTime.TabIndex = 0;
+            // 
+            // endTime
+            // 
+            this.endTime.Location = new System.Drawing.Point(280, 127);
+            this.endTime.Name = "endTime";
+            this.endTime.Size = new System.Drawing.Size(100, 29);
+            this.endTime.TabIndex = 1;
+            // 
+            // arrivalTime
+            // 
+            this.arrivalTime.Location = new System.Drawing.Point(280, 259);
+            this.arrivalTime.Name = "arrivalTime";
+            this.arrivalTime.Size = new System.Drawing.Size(100, 29);
+            this.arrivalTime.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(280, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 50);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1. Enter Old Start Time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2. Enter Old End Time:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "4. New Arrival Time:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(64, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "3. Click Here:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(538, 38);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exiToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(58, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // exiToolStripMenuItem
+            // 
+            this.exiToolStripMenuItem.Name = "exiToolStripMenuItem";
+            this.exiToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.exiToolStripMenuItem.Text = "Exit";
+            this.exiToolStripMenuItem.Click += new System.EventHandler(this.exiToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(240, 34);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 550);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.arrivalTime);
+            this.Controls.Add(this.endTime);
+            this.Controls.Add(this.startTime);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox startTime;
+        private System.Windows.Forms.TextBox endTime;
+        private System.Windows.Forms.TextBox arrivalTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Project03/Form1.cs
@@ -0,0 +1,77 @@
+﻿// Author: Jonathan Spalding
+// Assignment: Project 03
+// Instructor Roger deBry
+// Clas: CS 1400
+// Date Written: 2/7/2016
+//
+// "I declare that the following source code was written solely by me. I understand that copying any source code, in whole or in part, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy."
+//----------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The button1_Click Method
+        // Purpose: To add 25% transit time for a delivery in military time.
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //State All Constants
+            const int HUN_PART = 100;
+            const double DELAY = .25;
+            const int HOUR = 60;
+            //convert input from a string to integers.
+            int oldStart = int.Parse(startTime.Text);
+            int oldEnd = int.Parse(endTime.Text);
+            //split the hours and minutes for the start time, and convert the hours into minutes, then add it together.
+            int oldStartHours = (oldStart / HUN_PART) * HOUR;
+            int oldStartMins = oldStart % HUN_PART;
+            int oldStartTotal = oldStartHours + oldStartMins;
+            //repeat for the oldEndHours.
+            int oldEndHours = (oldEnd / HUN_PART) * HOUR;
+            int oldEndMins = oldEnd % HUN_PART;
+            int oldEndTotal = oldEndHours + oldEndMins;
+            //subtract the arrival time from the time left to find how long the transit is.
+            int arrivalTransit = oldEndTotal - oldStartTotal;
+            //multiply by .25 to add 25% delay.
+            int delayTime = Convert.ToInt32(Math.Floor(arrivalTransit * DELAY)); //I had to round down or else my answer was always 1 higher than the example program.
+            // add the delay plus the transit to the left time to find the new arrival time.
+            int arrivalTimeTotal = arrivalTransit + delayTime + oldStartTotal;
+            //to put it back in the military time format, divide it by 60, then multiply by 100, and then add on the minutes. (This is the opposite of what you do to split the minutes and hours before.
+            int total = arrivalTimeTotal / HOUR * HUN_PART + arrivalTimeTotal % HOUR;
+            //convert to a string, then output in the output box
+            string outStr = $"{total:D4}";
+            arrivalTime.Text = outStr;
+        }
+        // The exitToolStripMenuItem1_Click Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void exiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        // The aboutToolStripMenuItem_Click Method
+        // Purpose: Display a pop up box when you click About
+        // Parameters: The sending object, and the event arguments
+        // Returns: none
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Jonathan Spalding\nCS1400\nProjecct 03");
+        }
+    }
+}
